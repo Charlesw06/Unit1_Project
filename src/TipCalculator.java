f23e75aa986b1d94b51a04919620af1f7beee25f
BT_4N2_20 <cwalford3884@bths.edu>
2022-09-21 11:56:35 -0400

    Initial commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner billAmt = new Scanner(System.in);
+        System.out.print("Enter bill amount: ");
+        double bill =  billAmt.nextDouble();
+
+        Scanner
+    }
+}
