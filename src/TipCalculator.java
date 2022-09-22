4115b9d6c5567ec97d898c41be8a6b619ffc6166
Charlesw06 <cwalford3884@bths.edu>
2022-09-21 22:45:16 -0400

    Added decimal format to display money properly

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
+// Imports Scanner and DecimalFormat
 
 public class TipCalculator {
     public static void main(String[] args) {
+        DecimalFormat twoDForm = new DecimalFormat("#.00");
+        // Sets the decimal format to two digits after the decimal point
+
         Scanner billAmt = new Scanner(System.in);
         System.out.print("Enter bill amount: ");
         double bill = billAmt.nextDouble();
@@ -9,7 +14,7 @@ public class TipCalculator {
 
         Scanner percentOfTip = new Scanner(System.in);
         System.out.print("Enter tip percent: ");
-        double tipPercent = percentOfTip.nextDouble();
+        double tipPercent = Math.round(percentOfTip.nextDouble()) / 100.0;
         // Creates scanner for tip percent and prompts user
 
         Scanner numOfPeople = new Scanner(System.in);
@@ -17,17 +22,17 @@ public class TipCalculator {
         int numPeople = numOfPeople.nextInt();
         // Creates scanner for number of people and prompts user
 
-        double tipTotal = (Math.round(tipPercent / 10.0 * bill * 10.0) / 100.0);
+        double tipTotal = tipPercent * bill;
         double billTotal = bill + tipTotal;
         double tipPerPerson = tipTotal / numPeople;
         double costPerPerson = billTotal / numPeople;
         // Initializes variables for calculations
 
         System.out.println("------------------------");
-        System.out.println("Total tip: $" + tipTotal);
-        System.out.println("Total bill: $" + billTotal);
-        System.out.println("Tip per person: $" + tipPerPerson);
-        System.out.println("Cost per person: $" + costPerPerson);
-        // Displays the results to user
+        System.out.println("Total tip: $" + twoDForm.format(tipTotal));
+        System.out.println("Total bill: $" + twoDForm.format(billTotal));
+        System.out.println("Tip per person: $" + twoDForm.format(tipPerPerson));
+        System.out.println("Cost per person: $" + twoDForm.format(costPerPerson));
+        // Displays the results to user in proper money form
     }
 }
